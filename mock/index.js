7e9df51d712737b422865f23823a389a05ac3280
Yad Smood <y.s.inside@gmail.com>
2017-07-27 17:04:21 +0800

    fix: make all the html request return the same index page

--- mock/index.js
+++ mock/index.js
@@ -2,6 +2,8 @@ var defaultRoutes = require('febone-kit/lib/default-routes');
 
 module.exports = (app, opts) => {
     app.push(($) => {
+        if (/text\/html/.test($.req.headers.accept))
+            $.req.url = '/';
         return $.next();
     });
 
